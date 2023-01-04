507936312a53921437035844b8230cb3b0cfe34d
Mike van den Hoek <mike@yard.nl>
2023-01-04 14:01:22 +0100

    (feat): add vendor folder to versioning

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mvdhoek/sorrento-theme',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '614ebcaad669509ba663f47d4322aea32e73fb37',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mvdhoek/sorrento-theme' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '614ebcaad669509ba663f47d4322aea32e73fb37',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.7.1',
+            'version' => '3.7.1.0',
+            'reference' => '1359e176e9307e906dc3d890bcc9603ff6d90619',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
