d3241c217bdc4df254618bf234ed8bbf3bed1d93
Mike van den Hoek <mike@yard.nl>
2023-01-23 09:28:09 +0100

    (refactor): remove wrapping container class

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,7 @@
 <div class="main">
     @include('sections.header')
         <main id="main" class="main-content">
-            <div class="container">
-                @yield('content')
-            </div>
+            @yield('content')
         </main>
     @include('sections.footer')
 </div>
